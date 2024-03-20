9631565a08bec009c6d31cdf61a39681701e89e6
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-03-20 13:34:03 -0300

    Endpoint para retornar posts de um usuario

--- src/main/java/com/erickbonifacio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/erickbonifacio/workshopmongo/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erickbonifacio.workshopmongo.dto.UserDTO;
+import com.erickbonifacio.workshopmongo.entities.Post;
 import com.erickbonifacio.workshopmongo.entities.User;
 import com.erickbonifacio.workshopmongo.services.UserService;
 
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
