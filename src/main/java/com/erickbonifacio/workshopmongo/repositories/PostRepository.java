595d92dc185280d0502e0b745dc421dc1d597c6c
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-03-20 20:00:21 -0300

    Consulta simples com @Query

--- src/main/java/com/erickbonifacio/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/erickbonifacio/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.erickbonifacio.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.erickbonifacio.workshopmongo.entities.Post;
@@ -11,4 +12,7 @@ import com.erickbonifacio.workshopmongo.entities.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{'title': { $regex: ?0, $options: 'i' }}")
+	List<Post> findByTitle(String text);
 }
